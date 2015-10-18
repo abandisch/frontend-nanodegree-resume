d7badac33f2d76d6c59b30568277bddcbbb61bef
Alex Bandisch <alex@cybermasta.com>
2015-10-18 14:13:07 +1000

    Ensures the role is displays responsively

--- js/helper.js
+++ js/helper.js
@@ -15,7 +15,7 @@ var fontAwesomeIcon = '<i class="fa %data%"></i>';
  replace the %data% placeholder text you see in them.
  */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span id="role">%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
