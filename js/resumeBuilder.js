32860922df86d11d0b32d01d8c15b37ae2b8f796
Alex Bandisch <alex@cybermasta.com>
2015-10-18 13:25:03 +1000

    Cleans up the code and commetns for js/resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,79 +1,81 @@
-/*
- This is empty on purpose! Your code to build the resume will go here.
- */
-
-/* Header */
-var myName = "Alex Bandisch";
-var myRole = "Front-End Web Developer";
-
-$("#header").prepend(HTMLheaderRole.replace("%data%", myRole));
-$("#header").prepend(HTMLheaderName.replace("%data%", myName));
-
-/* Bio */
+// Bio JSON
 var bio = {
     "name": "Alex Bandisch",
     "role": "Front-End Web Developer",
-    "welcomeMessage": "An ambitious problem solver with a passion for Front-End Web Development!",
     "contacts": {
         "mobile": "+61 433 281 488",
         "email": "alexander.bandisch@gmail.com",
-        "location": "Brisbane, Australia",
-        "github": "https://github.com/abandisch"
+        "github": "https://github.com/abandisch",
+        "twitter": "https://twitter.com/abandisch",
+        "location": "Brisbane, Australia"
     },
-    "pictureURL": "http://i.imgur.com/CXkInEz.jpg",
-    "skills": ["Bootstrap 3", "HTML", "JQuery", "CSS", "less", "PHP", "GruntJS", "LAMP Stack"]
+    "welcomeMessage": "An ambitious problem solver with a passion for Front-End Web Development!",
+    "skills": ["Bootstrap 3", "HTML", "JQuery", "CSS", "less", "PHP", "GruntJS", "LAMP Stack"],
+    "biopic": "http://i.imgur.com/CXkInEz.jpg"
+
 };
 
+// contactIcons JSON: this is used for the contact details on the footer (replaces the label text with Font Awesome icons)
 var contactIcons = {
     "location": "fa-map-marker",
+    "twitter": "fa-twitter",
     "email": "fa-envelope",
     "mobile": "fa-mobile",
     "github": "fa-github"
 };
 
-/* Contact Info */
-$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email))
-    .append(HTMLmobile.replace("%data%", bio.contacts.mobile))
-    .append(HTMLlocation.replace("%data%", bio.contacts.location))
-    .append(HTMLgithub.replace("%data%", bio.contacts.github));
+bio.display = function() {
+    // Page Title
+    var titleText = $("title").text();
+    $("title").text(titleText + " - " + bio.name);
 
-/* Footer contacts with Font Awesome icons instead of text */
-var $footerContacts = $("#footerContacts");
-$.each(bio.contacts, function(contactType, contactData) {
-    var icon = fontAwesomeIcon.replace("%data%", contactIcons[contactType]);
-    var formattedContact = HTMLcontactGeneric.replace("%contact%", icon).replace("%data%", contactData);
-    $footerContacts.append(formattedContact);
-});
+    // Name and Role
+    $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+    $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
 
-/* Image */
-$("#header").append(HTMLbioPic.replace("%data%", bio.pictureURL));
+    // Contact Info
+    $("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email))
+                     .append(HTMLmobile.replace("%data%", bio.contacts.mobile))
+                     .append(HTMLlocation.replace("%data%", bio.contacts.location))
+                     .append(HTMLgithub.replace("%data%", bio.contacts.github))
+                     .append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
 
-/* Welcome Message */
-$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+    // Bio Picture
+    $("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
 
-/* Skills */
-if (bio.skills.length > 0) {
-    $('#header').append(HTMLskillsStart);
-    $(bio.skills).each(function(index, element) {
-        $('#skills').append(HTMLskills.replace("%data%", element));
+    // Welcome Message
+    $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+
+    // Skills
+    if (bio.skills.length > 0) {
+        $('#header').append(HTMLskillsStart);
+        $(bio.skills).each(function(index, element) {
+            $('#skills').append(HTMLskills.replace("%data%", element));
+        });
+    }
+
+    // Footer contacts: show with Font Awesome icons instead of text
+    var $footerContacts = $("#footerContacts");
+    $.each(bio.contacts, function(contactType, contactData) {
+        var icon = fontAwesomeIcon.replace("%data%", contactIcons[contactType]);
+        var formattedContact = HTMLcontactGeneric.replace("%contact%", icon).replace("%data%", contactData);
+        $footerContacts.append(formattedContact);
     });
-    $('#skills').removeClass("flex-box");
-    $('#skills').find("li").removeClass("flex-item");
-}
 
+};
 
-/* Education */
-var formattedSchool, formattedOnlineCourse, education, $educationDiv = $("#education");;
-education = {
+// Education JSON
+var formattedSchool, formattedOnlineCourse, $educationDiv = $("#education");
+var education = {
     "schools" : [
         {
             "name": "USQ",
-            "degree": "Bachelor of Information Technology",
-            "dates": "2002 - 2004",
             "location": "Toowoomba, Australia",
+            "degree": "Bachelor of Information Technology",
             "majors": [
                 "Networking"
             ],
+            "dates": 2004,
             "url" : "http://usq.edu.au"
         }
     ],
@@ -81,48 +83,54 @@ education = {
         {
             "title": "Front-End Web Developer Nanodegree",
             "school": "Udacity",
-            "dates": "August 2015 - March 2016",
+            "date": 2016,
             "url": "https://www.udacity.com/"
         }
     ]
 };
 
-$(education.schools).each(function(index, schoolElement) {
-    formattedSchool = $(HTMLschoolStart)
-        .append(HTMLschoolName.replace("%data%", schoolElement.name) + HTMLschoolDegree.replace("%data%", schoolElement.degree))
-        .append(HTMLschoolDates.replace("%data%", schoolElement.dates))
-        .append(HTMLschoolLocation.replace("%data%", schoolElement.location))
-        .append(HTMLschoolMajor.replace("%data%", schoolElement.majors.join(", ")));
-    $educationDiv.append(formattedSchool);
-});
+education.display = function() {
+    // Add schools
+    $(education.schools).each(function(index, schoolElement) {
+        formattedSchool = $(HTMLschoolStart).append(HTMLschoolName.replace("%data%", schoolElement.name) + HTMLschoolDegree.replace("%data%", schoolElement.degree))
+                                            .append(HTMLschoolDates.replace("%data%", schoolElement.dates))
+                                            .append(HTMLschoolLocation.replace("%data%", schoolElement.location));
+        if (schoolElement.majors.length > 0) {
+            formattedSchool.append(HTMLschoolMajor.replace("%data%", schoolElement.majors.join(", ")));
+        }
+        $educationDiv.append(formattedSchool);
+    });
 
-$educationDiv.append(HTMLonlineClasses);
+    // Add online courses
+    if (education.onlineCourses.length > 0) {
+        $educationDiv.append(HTMLonlineClasses);
 
-$(education.onlineCourses).each(function(index, onlineCourseElement) {
-    formattedOnlineCourse = $(HTMLschoolStart)
-        .append(HTMLonlineTitle.replace("%data%", onlineCourseElement.title) + HTMLonlineSchool.replace("%data%", onlineCourseElement.school))
-        .append(HTMLonlineDates.replace("%data%", onlineCourseElement.dates))
-        .append(HTMLonlineURL.replace("%data%", onlineCourseElement.url));
-    $educationDiv.append(formattedOnlineCourse);
-});
+        $(education.onlineCourses).each(function(index, onlineCourseElement) {
+            formattedOnlineCourse = $(HTMLschoolStart).append(HTMLonlineTitle.replace("%data%", onlineCourseElement.title) + HTMLonlineSchool.replace("%data%", onlineCourseElement.school))
+                                                      .append(HTMLonlineDates.replace("%data%", onlineCourseElement.date))
+                                                      .append(HTMLonlineURL.replace("%data%", onlineCourseElement.url));
+            $educationDiv.append(formattedOnlineCourse);
+        });
+    }
+};
 
-/* Work */
+// Work JSON
 var $formattedWork, work;
 
 work = {
     "jobs" : [
         {
-            "title": "Project Manager",
             "employer": "PNGConnect",
-            "dates": "2014 - Current",
+            "title": "Project Manager",
             "location": "Brisbane, Australia",
+            "dates": "2014 - Current",
             "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla posuere sem est, nec sagittis nunc aliquet ac. Quisque convallis dui a erat porttitor, vel faucibus magna ultricies. Vestibulum quis dapibus metus"
         },
         {
-            "title": "Implementation Consultant",
             "employer": "GBST",
-            "dates": "2006 - 2014",
+            "title": "Implementation Consultant",
             "location": "Brisbane, Australia",
+            "dates": "2006 - 2014",
             "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla posuere sem est, nec sagittis nunc aliquet ac. Quisque convallis dui a erat porttitor, vel faucibus magna ultricies. Vestibulum quis dapibus metus"
         }
     ]
@@ -140,12 +148,8 @@ work.display = function() {
     }
 };
 
-work.display();
-
-/* Projects */
-var formattedProject, projects;
-
-projects = [
+// Projects JSON
+var projects = [
     {
         "title": "About Me - Front-End Apprentice",
         "dates": "September 2015",
@@ -161,34 +165,42 @@ projects = [
 ];
 
 projects.display = function() {
+    // Add each project to the resume
+    $(this).each(function(index, projectElement) {
+        var formattedProject = $(HTMLprojectStart).append(HTMLprojectTitle.replace("%data%", projectElement.title))
+                                                  .append(HTMLprojectDates.replace("%data%", projectElement.dates))
+                                                  .append(HTMLprojectDescription.replace("%data%", projectElement.description));
 
-    $(this).each(function(index, element) {
-        formattedProject = $(HTMLprojectStart)
-            .append(HTMLprojectTitle.replace("%data%", element.title))
-            .append(HTMLprojectDates.replace("%data%", element.dates))
-            .append(HTMLprojectDescription.replace("%data%", element.description));
-
-        if (element.image.length > 0) {
-            formattedProject.append(HTMLprojectImage.replace("%data%", element.image));
+        if (projectElement.image.length > 0) {
+            formattedProject.append(HTMLprojectImage.replace("%data%", projectElement.image));
         }
 
         $("#projects").append(formattedProject);
     });
 
-    /* Display Udacity projects next to each other rather than stacked */
+    // Add CSS to display projects next to each other rather than stacked
     $("#projects").addClass("flex-box");
     $("#projects").find(".project-entry").addClass("flex-item");
     $("#projects").find("h2").addClass("flex-item");
 };
 
+// Call the display functions for each object to display the resume information
+bio.display();
+work.display();
 projects.display();
+education.display();
 
-/* Google Map */
+// Display Google Map with bouncing markers on each location
 $("#mapDiv").append(googleMap);
 
-/* International Button */
 
-/* // Commented out ... don't think this is needed for final resume project
+
+
+
+
+
+/* International Button Exercise - Commented out, as this is not required for the resume ... at least I'm pretty sure it's not */
+/*
  $("#header").append(internationalizeButton);
  function inName() {
 
@@ -208,5 +220,6 @@ $("#mapDiv").append(googleMap);
 
  return finalName;
 
- }*/
+ }
+ */
 
